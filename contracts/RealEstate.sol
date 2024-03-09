3b213944895f059382dfc77d9f46c5bc7c6280d6
Sajgroundhogday <sajgroundhogday@gmail.com>
2024-03-09 13:26:56 +0530

    created a minting function for the NFT

--- contracts/RealEstate.sol
+++ contracts/RealEstate.sol
@@ -1,27 +1,29 @@
 //SPDX-License-Identifier: Unlicense
 pragma solidity ^0.8.0;
 
-import "@openzeppelin/contracts/utils/Counters.sol";
-import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
-import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
+import {ERC721URIStorage} from "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
+import {Counters} from "@openzeppelin/contracts/utils/Counters.sol";
+import {ERC721} from "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 
-contract RealEstate is ERC721URIStorage {
+contract RealEstate is ERC721URIStorage{
+
+    //for minting purpose.
     using Counters for Counters.Counter;
     Counters.Counter private _tokenIds;
 
-    constructor() ERC721("Real Estate", "REAL") {}
+    constructor() ERC721("Real Estate", "Real") {}  
 
-    function mint(string memory tokenURI) public returns (uint256) {
+    function mint() public returns (uint) {
         _tokenIds.increment();
 
-        uint256 newItemId = _tokenIds.current();
+        uint newItemId = _tokenIds.current();
         _mint(msg.sender, newItemId);
         _setTokenURI(newItemId, tokenURI);
 
         return newItemId;
     }
 
-    function totalSupply() public view returns (uint256) {
+    function totalSupply() public view returns (uint) {
         return _tokenIds.current();
     }
 }
